3fbc6236296fcc37a2e9a38ec6d5ab9d7f23b2bf
LizzyKate <onyenekwelizabeth@gmail.com>
2024-10-05 03:00:26 +0100

    test with jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,54 @@
+const request = require("supertest");
+const app = require("../../app");
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app).get("/launches").expect(200);
+    expect(response.body).toBeInstanceOf(Array);
+  });
+});
+
+describe("Test POST /launch", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D ",
+        destination: "Kepler-186 f",
+        launchDate: "2022-10-01",
+      })
+      .expect(201);
+    const requestDate = new Date(response.body.launchDate);
+    const responseDate = new Date(response.body.launchDate);
+    expect(requestDate).toEqual(responseDate);
+  });
+
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        destination: "Kepler-186 f",
+        launchDate: "2022-10-01",
+      })
+      .expect(400);
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
+  });
+
+  test("It should catch invalid data", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D ",
+        destination: "Kepler-186 f",
+        launchDate: "invalid date",
+      })
+      .expect(400);
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date",
+    });
+  });
+});
