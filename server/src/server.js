9fc1fe6941a519c6ef66417e25e64001d8a3176f
LizzyKate <onyenekwelizabeth@gmail.com>
2024-12-04 16:07:24 +0100

    dotenv

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+require("dotenv").config();
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model.js");
 const { mongoConnect } = require("./services/mongo.js");
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const MONGO_URL =
-  "mongodb+srv://Lizzy:1234567890@cluster0.pgusj.mongodb.net/nasa?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URL = process.env.MONGO_URL;
 
 async function mongoConnect() {
   await mongoose.connect(MONGO_URL);
